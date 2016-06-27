22c79ffef5402ce4d263503fc31a927b463eaff3
R. S. Doiel <rsdoiel@gmail.com>
2016-06-27 13:17:52 -0700

    Added Bib example and support for more complicated tokenization

--- tok.go
+++ tok.go
@@ -43,16 +43,50 @@ const (
 	// Version of  tok package
 	Version = "0.0.0"
 
-	//Base token typese
+	//Base token types, these are single letter tokens, no look ahead needed
 
 	// Letter is an alphabetical letter (e.g. A-Z, a-z in English)
 	Letter = "Letter"
-	// Numeral is a number (e.g. 1, 1.1) with up to one period, does not include fractional notation (E.g. 1/2, 3/4)
+	// Numeral is a single digit
 	Numeral = "Numeral"
 	// Punctuation is any non-number, non alphametical character, non-space (e.g. periods, colons, bang, hash mark)
 	Punctuation = "Punctuation"
 	// Space characters representing white space (e.g. space, tab, new line, carriage return)
 	Space = "Space"
+
+	// These are some common specialized token types provided for convientent.
+
+	// Words a sequence of characters delimited by spaces
+	Word = "Word"
+	// OpenCurly bracket, e.g. "{"
+	OpenCurlyBracket = "OpenCurlyBracket"
+	// CloseCurly bracket, e.g. "}"
+	CloseCurlyBracket = "CloseCurlyBracket"
+	// CurlyBracket, e.g. "{}"
+	CurlyBracket = "CurlyBracket"
+	// OpenSquareBracket, e.g. "["
+	OpenSquareBracket = "OpenSquareBracket"
+	// CloseSquareBracket, e.g. "]"
+	CloseSquareBracket = "CloseSquareBracket"
+	// SquareBracket, e.g. "[]"
+	SquareBracket = "SquareBracket"
+	// OpenAngleBracket, e.g. "<"
+	OpenAngleBracket = "OpenAngleBracket"
+	// CloseAngleBracket, e.g. ">"
+	CloseAngleBracket = "CloseAngleBracket"
+	// AngleBracket, e.g. "<>"
+	AngleBracket = "AngleBracket"
+	// AtSign, e.g. "@"
+	AtSign = "AtSign"
+	// EqualSign, e.g. "="
+	EqualSign = "EqualSign"
+	// DoubleQuote, e.g. "\""
+	DoubleQuote = "DoubleQuote"
+	// SingleQuote, e.g., "'"
+	SingleQuote = "SingleQuote"
+
+	// EOF is an end of file token type. It is separate form Space only because of it being a common stop condition
+	EOF = "EOF"
 )
 
 var (
@@ -63,7 +97,37 @@ var (
 	Spaces = []byte(" \t\r\n")
 
 	// PunctuationMarks map as strings
-	PunctuationMarks = []byte("~!@#$%^&*()-+`_:{}|[]\\:;\"'><?,./")
+	PunctuationMarks = []byte("~!@#$%^&*()_+`-=:{}|[]\\:;\"'<>?,./")
+
+	// These map to the specialized tokens
+	AtSignMark = []byte("@")
+	// EqualMark, e.g. =
+	EqualMark = []byte("=")
+	// DoubleQuoteMark, e.g. "\""
+	DoubleQuoteMark = []byte("\"")
+	// SingleQuoteMark, e.g. "'"
+	SingleQuoteMark = []byte("'")
+
+	// OpenCurlyBrackets token
+	OpenCurlyBrackets = []byte("{")
+	// CloseCurlyBrackets token
+	CloseCurlyBrackets = []byte("}")
+	// CurlyBrackets tokens
+	CurlyBrackets = []byte("{}")
+
+	// OpenSquareBrackets token
+	OpenSquareBrackets = []byte("[")
+	// CloseSquareBrackets token
+	CloseSquareBrackets = []byte("]")
+	// SquareBrackets tokens
+	SquareBrackets = []byte("[]")
+
+	// OpenAngleBrackets token
+	OpenAngleBrackets = []byte("<")
+	// CloseAngleBrackets token
+	CloseAngleBrackets = []byte(">")
+	// AngleBrackets tokens
+	AngleBrackets = []byte("<>")
 )
 
 // Token structure for emitting simply tokens and value from Tok() and Tok2()
@@ -72,6 +136,9 @@ type Token struct {
 	Type    string   `xml:"type" json:"type"`
 	Value   []byte   `xml:"value" json:"value"`
 }
+
+// TokenMap is a map of simple token names and associated array of possible bytes
+type TokenMap map[string][]byte
 
 // Tokenizer is a function that takes a current token, looks ahead in []byte and returns a revised token and remaining []byte
 type Tokenizer func(*Token, []byte) (*Token, []byte)
@@ -96,6 +163,11 @@ func IsNumeral(b []byte) bool {
 	return bytes.Contains(Numerals, b)
 }
 
+// IsTypeOf checks to see if a []byte is a specific type
+func IsTypeOf(b []byte, typeMap []byte) bool {
+	return bytes.Contains(typeMap, b)
+}
+
 // Tok is a naive tokenizer that looks only at the next character by shifting it off the []byte and returning a token found with remaining []byte
 func Tok(buf []byte) (*Token, []byte) {
 	var (
@@ -103,7 +175,7 @@ func Tok(buf []byte) (*Token, []byte) {
 	)
 	if len(buf) == 0 {
 		return &Token{
-			Type:  "EOF",
+			Type:  EOF,
 			Value: []byte(""),
 		}, nil
 	}
@@ -132,6 +204,18 @@ func Tok(buf []byte) (*Token, []byte) {
 	}
 }
 
+// TokenFromMap, revaluates token type against a map of type names and byte arrays
+// returns modified Token
+func TokenFromMap(t *Token, m map[string][]byte) *Token {
+	for k, v := range m {
+		if bytes.Contains(v, t.Value) {
+			t.Type = k
+			return t
+		}
+	}
+	return t
+}
+
 // Tok2 provides an easy to implement look ahead tokenizer by defining a look ahead function
 func Tok2(buf []byte, fn Tokenizer) (*Token, []byte) {
 	tok, rest := Tok(buf)
@@ -140,14 +224,49 @@ func Tok2(buf []byte, fn Tokenizer) (*Token, []byte) {
 
 // Words is an example of implementing a Tokenizer function
 func Words(tok *Token, buf []byte) (*Token, []byte) {
-	if tok.Type == Letter || tok.Type == "Word" {
+	if tok.Type == Letter || tok.Type == Word {
 		// Get the next Token
 		newTok, newBuf := Tok(buf)
 		if newTok.Type == Letter {
-			tok.Type = "Word"
+			tok.Type = Word
 			tok.Value = append(tok.Value, newTok.Value[0])
 			tok, buf = Words(tok, newBuf)
 		}
 	}
+	return tok, buf
+}
+
+// Bib is an example of a niave BibTeX Tokenizer function
+func Bib(tok *Token, buf []byte) (*Token, []byte) {
+	switch {
+	case tok.Type == AtSign || tok.Type == "BibElement":
+		// Get the next Token
+		newTok, newBuf := Tok(buf)
+		if newTok.Type == Letter {
+			tok.Type = "BibElement"
+			tok.Value = append(tok.Value, newTok.Value[0])
+			tok, buf = Bib(tok, newBuf)
+		}
+	case tok.Type == Letter || tok.Type == Word:
+		// Get the next Token
+		newTok, newBuf := Tok(buf)
+		if newTok.Type == Letter {
+			tok.Type = Word
+			tok.Value = append(tok.Value, newTok.Value[0])
+			tok, buf = Bib(tok, newBuf)
+		}
+	default:
+		// Revaluate token for more specific token types.
+		tok = TokenFromMap(tok, map[string][]byte{
+			OpenCurlyBracket:  OpenCurlyBrackets,
+			CloseCurlyBracket: CloseCurlyBrackets,
+			AtSign:            AtSignMark,
+			EqualSign:         EqualMark,
+			DoubleQuote:       DoubleQuoteMark,
+			SingleQuote:       SingleQuoteMark,
+			"Comma":           []byte(","),
+		})
+	}
+
 	return tok, buf
 }
